c7dffd1d8a95cd1eeca2d58763edaa6c62419de1
Shane Bendak <shebah712@gmail.com>
2020-07-17 14:09:52 -0700

    [variables] add 5 variations

---
+++ app.js
@@ -0,0 +1,26 @@
+//5 variables of different types
+//string
+//number
+//array
+//boolean
+//object (4 different key/value pairs) [string], [number], [array], [boolean]
+
+var string = "This is my string!";
+console.log(string);
+
+var number = 67;
+console.log(number);
+
+var array = [1, 2, 3, 4];
+console.log(array);
+
+var boolean = false;
+console.log(boolean);
+
+var object = {
+    str: "obj string",
+    num: 33,
+    objArray: [1, 2, 3, 4],
+    objBoolean: false
+}
+console.log(object);
